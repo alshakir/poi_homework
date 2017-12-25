8e5b1cec3c2a1b2c86d28bdcb8d7e652ff14fbce
shakir <alshakir_sa@yahoo.com>
2017-12-25 14:50:48 +0300

    code cleaning

--- poi_id_final.py
+++ poi_id_final.py
@@ -97,31 +97,7 @@ for i in my_dataset.keys():
 data = feature_format.featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = feature_format.targetFeatureSplit(data)
 
-# print 'length of data_dict: ',len(data_dict)
-# for i in my_dataset['METTS MARK'].keys():
-#     print i
-#     print my_dataset['METTS MARK'][i]
-# print
-# print my_dataset['METTS MARK']
-# print
-# print '****************'
-# print 
 
-
-
-
-#print data
-# print 'length of data',len(data)
-
-
-# print 'length of labels : ', len(labels),'^^^^^^^^^^^^^^^^^^^^^^^^^^^^^'
-# print 'now following is lables: ......\n\n\n'
-# print labels
-
-# print 'length of features :' , len(features)
-# print 'Now following is features: ...\n\n\n'
-# print features
-# print 'End of cell 5'
 
 
 
@@ -136,10 +112,6 @@ labels, features = feature_format.targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
-
-print 'This is gaussianNB without using KFold'
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -154,53 +126,6 @@ features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
 
-clf.fit(features_train,labels_train)
-
-pred = clf.predict(features_test)
-
-
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-from sklearn.metrics import recall_score, precision_score
-
-
-score = accuracy_score(labels_test,pred)
-
-prec_reca_f = precision_recall_fscore_support(labels_test,pred)
-
-print score
-print '----------------'
-
-print prec_reca_f
-print 'end of cell 6'
-
-
-print recall_score(labels_test,pred), ' is the recall'
-print precision_score(labels_test, pred), 'is the precision'
-
-
-
-print labels_test[7]
-print pred[7]
-counter = 0
-wrongcount=0
-
-cc = 1
-
-for i in labels_test:
-    if i == 1 or pred[counter]== 1 :
-        print 'i = ', i,  ' ___pred = ', pred[counter]
-        print cc
-        cc+=1
-    # if i == pred[counter]:
-    #     print i, '****', pred[counter]
-    #     print type(i),'type***', type(pred[counter])
-    #     print counter
-    # else:
-    #     wrongcount+=1
-    counter+=1
-
-print counter
-print 'wrong count = ', wrongcount
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
@@ -210,7 +135,7 @@ dump_classifier_and_data(clf, my_dataset, features_list)
 
 print 'end of cell 6'
 
-#%% cell 7
+
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -221,23 +146,8 @@ print 'end of cell 6'
 # Provided to give you a starting point. Try a variety of classifiers.
 
 
-print 'end of cell 7'
 
 
-
-
-#%% cell testing all classifiers
-
-useGaussianNB()
-
-#useSVM()
-useDTClf()
-print 'end of cell testing all classifiers'
-
-
-#%%
-
-useGaussianNB()
 
 
 #%%
@@ -246,245 +156,47 @@ print features_list
 #%% cell 11
 # testing gaussianNB with kfold
 
-def useGaussianNBKfold(labels,features):
-    # print "This is the useGaussianNBKfold() method"
-    import numpy as np
-    from sklearn.naive_bayes import GaussianNB
-    from sklearn.model_selection import KFold,StratifiedKFold
 
-    from sklearn.cross_validation import cross_val_score
+# print "This is the useGaussianNBKfold() method"
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import KFold,StratifiedKFold
 
-    from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-    from sklearn.metrics import recall_score, precision_score
-    import numpy as np
+from sklearn.cross_validation import cross_val_score
 
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from sklearn.metrics import recall_score, precision_score
+import numpy as np
 
-    kf = KFold(n_splits=7,shuffle=True)
-    clf = GaussianNB()
-    # print type(features)
 
-    # print '-----------\n\n\n'
+kf = KFold(n_splits=7,shuffle=True)
+clf = GaussianNB()
+# print type(features)
 
-    # print cross_val_score(clf,features,labels,scoring=None,cv=7)
-    # print '-----------\n\n\n'
-    # print '-----------\n\n\n'
-    
+# print '-----------\n\n\n'
 
-    f = np.array(features)
-    l = np.array(labels)
-    # print f.shape
-    # print l.shape
+# print cross_val_score(clf,features,labels,scoring=None,cv=7)
+# print '-----------\n\n\n'
+# print '-----------\n\n\n'
 
-    recList = []
-    presList = []
 
-    for train_index, test_index in kf.split(features):
-        # print train_index
-        # print '====='
-        # print test_index
-        # print'*********\n\n'
-        features_train = f[train_index]
-        labels_train = l[train_index]
-        features_test = f[test_index]
-        labels_test = l[test_index]
+f = np.array(features)
+l = np.array(labels)
+# print f.shape
+# print l.shape
 
-  
-        clf.fit(features_train,labels_train)
+recList = []
+presList = []
 
-        pred = clf.predict(features_test)
-
-
-        score = accuracy_score(labels_test,pred)
-
-        prec_reca_f = precision_recall_fscore_support(labels_test,pred)
-
-        # print score
-        # print '----------------'
-
-        # print prec_reca_f
-        # print 'end of cell 6'
-
-        rec =  recall_score(labels_test,pred)
-        pres = precision_score(labels_test, pred)
-        # print rec, ' is the recall'
-        # print pres, 'is the precision'
-        recList.append(rec)
-        presList.append(pres)
-
-
-        # print labels_test[7]
-        # print pred[7]
-        counter = 0
-        wrongcount=0
-
-        cc = 1
-
-        # for i in labels_test:
-        #     if i == 1 or pred[counter]== 1 :
-        #         print 'i = ', i,  ' ___pred = ', pred[counter]
-        #         print cc
-        #         cc+=1
-        #     # if i == pred[counter]:
-        #     #     print i, '****', pred[counter]
-        #     #     print type(i),'type***', type(pred[counter])
-        #     #     print counter
-        #     # else:
-        #     #     wrongcount+=1
-        #     counter+=1
-
-        # print counter
-        # print 'wrong count = ', wrongcount
-    avgRecall = np.average(recList)
-    avgPrecision = np.average(presList)
-
-    findTheBest(avgRecall,avgPrecision)
-    # print "average recall =  ", avgRecall
-    # print "average precision  = " , avgPrecision
-
-    
-# useGaussianNBKfold()
-print 'end of cell 11'
-
-listBest =[[],0,1]
-def findTheBest(rec,prec):
-    import numpy as np
-    sumRecPrec = rec + prec
-    diffRecPrec = np.abs(rec - prec)
-    global listBest
-    theSum = listBest[1]
-    theDiff = listBest[2]
-
-    if(sumRecPrec > theSum and diffRecPrec < theDiff):
-        listBest[0] = thefinalList
-        listBest[1] = sumRecPrec
-        listBest[2] = diffRecPrec
-        print listBest
-        print 'recall = ',rec, ' And precision = ', prec
-      
-
-    
-
-#%% cell12
-def usePCA():
-    
-    print ' this is PCA with DTClf no piping'
-    from sklearn.decomposition import PCA
-
-    myPCA = PCA(n_components=3)
-    transformed_pca_features_train= myPCA.fit_transform(features_train)
-
-    transformed_pca_features_test = myPCA.fit_transform(features_test)
-    from sklearn.tree import DecisionTreeClassifier
-    clf = DecisionTreeClassifier()
-    clf.fit(transformed_pca_features_train,labels_train)
-
-    pred = clf.predict(transformed_pca_features_test)
-
-
-
-
-
-    from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-    from sklearn.metrics import recall_score, precision_score
-
-
-
-    print recall_score(labels_test,pred), ' is the recall'
-    print precision_score(labels_test, pred), 'is the precision'
-
-
-
-
-
-
-
-
-usePCA()
-print 'end of cell 12'
-
-
-
-
-#%% cell 13
-
-def usePCA_SVM():
-    
-    print 'this is pca svm piping and gridsearch'
-    from sklearn.decomposition import PCA
-    from sklearn.pipeline import Pipeline
-    from sklearn.model_selection import GridSearchCV
-    from sklearn import svm
-
-    params = {'pca__n_components':[2,3],
-    'svm__kernel': ['linear', 'rbf'] ,
-    'svm__C':[1,2,3,100,1000]}
-
-
-    from sklearn.preprocessing import MinMaxScaler
-    scaler = MinMaxScaler()
-
-    scaled_features_train = scaler.fit_transform(features_train)
-    scaled_features_test = scaler.fit_transform(features_test)
-
-    print 'datamax of scaler = ', scaler.data_max_
-
-    print scaled_features_train[:3]
-    
-    pipe = Pipeline(steps=[('pca', PCA()), ('svm', svm.SVC(max_iter=1000000))])
-    clf = GridSearchCV(pipe, params )
-
-
-
-    clf.fit(scaled_features_train,labels_train)
-
-    pred = clf.predict(scaled_features_test)
-
-
-
-
-
-    from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-    from sklearn.metrics import recall_score, precision_score
-
-
-
-    print recall_score(labels_test,pred), ' is the recall'
-    print precision_score(labels_test, pred), 'is the precision'
-
-usePCA_SVM()
-print 'end of cell 13'
-
-
-
-
-
-#%% cell14
-def usePCAKnearst():
-    print 'this is the usePCAKnearest method'
-    from sklearn.decomposition import PCA
-
-    myPCA = PCA(n_components=3)
-    transformed_pca_features_train= myPCA.fit_transform(features_train)
-
-    transformed_pca_features_test = myPCA.fit_transform(features_test)
-    from sklearn.neighbors import KNeighborsClassifier
-    # clf = KNeighborsClassifier()
-    # clf.fit(transformed_pca_features_train,labels_train)
-
-    # pred = clf.predict(transformed_pca_features_test)
-
-
-    from sklearn.model_selection import GridSearchCV
-    from sklearn.pipeline import Pipeline
-    
-    params = {'pca__n_components':[2,3],
-    'knearest__algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']}
-
-
-    
-
-    pipe = Pipeline(steps=[('pca', PCA()), ('knearest', KNeighborsClassifier())])
-    clf = GridSearchCV(pipe, params )
+for train_index, test_index in kf.split(features):
+    # print train_index
+    # print '====='
+    # print test_index
+    # print'*********\n\n'
+    features_train = f[train_index]
+    labels_train = l[train_index]
+    features_test = f[test_index]
+    labels_test = l[test_index]
 
 
     clf.fit(features_train,labels_train)
@@ -492,119 +204,55 @@ def usePCAKnearst():
     pred = clf.predict(features_test)
 
 
+    score = accuracy_score(labels_test,pred)
 
-    from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-    from sklearn.metrics import recall_score, precision_score
+    prec_reca_f = precision_recall_fscore_support(labels_test,pred)
 
+    # print score
+    # print '----------------'
 
+    # print prec_reca_f
+    # print 'end of cell 6'
 
-    print recall_score(labels_test,pred), ' is the recall'
-    print precision_score(labels_test, pred), 'is the precision'
-
-
-
-
-
-
-
-
-usePCAKnearst()
-print 'end of cell 14'
+    rec =  recall_score(labels_test,pred)
+    pres = precision_score(labels_test, pred)
+    print rec, ' is the recall'
+    print pres, 'is the precision'
 
 
+    recList.append(rec)
+    presList.append(pres)
 
 
-#%% cell 18
-thefinalList = ['poi']
-def combination(l):
-    ggg = 0
-    
+    # print labels_test[7]
+    # print pred[7]
+    counter = 0
+    wrongcount=0
 
-    global thefinalList 
-    thefinalList = ['poi']
-    theLength= len(l)
-    print (theLength)
+    cc = 1
 
-    for i in range(theLength):
-        
-        
-        thefinalList.append(l[i])
-        for j in range(i+1,theLength):
-            thefinalList.append(l[j])
-            for k in range(j+1,theLength):
-                thefinalList.append(l[k])
+    # for i in labels_test:
+    #     if i == 1 or pred[counter]== 1 :
+    #         print 'i = ', i,  ' ___pred = ', pred[counter]
+    #         print cc
+    #         cc+=1
+    #     # if i == pred[counter]:
+    #     #     print i, '****', pred[counter]
+    #     #     print type(i),'type***', type(pred[counter])
+    #     #     print counter
+    #     # else:
+    #     #     wrongcount+=1
+    #     counter+=1
 
-                # print thefinalList
-                ggg = ggg + 1
-                labels,features = loadFeaturesAndLabels(thefinalList)
-                useGaussianNBKfold(labels,features)
-                #do the job
-                thefinalList.pop()
-            thefinalList.pop()
-        thefinalList.pop()
-    print ggg
-    global listBest
-    listBest =[[],0,1]
+    # print counter
+    # print 'wrong count = ', wrongcount
+avgRecall = np.average(recList)
+avgPrecision = np.average(presList)
 
 
-
-myList = ['from_poi_percentage','to_poi_percentage','salary','total_payments', 'exercised_stock_options',
-    'bonus', 'restricted_stock', 'restricted_stock_deferred', 'total_stock_value',
-    'expenses', 'loan_advances', 'director_fees', 'deferred_income', 'long_term_incentive'] # You will need to use more features
+print "average recall =  ", avgRecall
+print "average precision  = " , avgPrecision
 
 
-combination(myList)
- 
-print 'end of cell 18'
-
-
-
-
-#%%
-
-def loadFeaturesAndLabels(features_list):
-
-    reload(feature_format)
-    ### Task 2: Remove outliers
-    ### Task 3: Create new feature(s)
-    ### Store to my_dataset for easy export below.
-    my_dataset = data_dict
-
-    #---------- adding new features----------
-
-    for i in my_dataset.keys():
-        x = float( my_dataset[i]['to_messages'])
-        y = float(my_dataset[i]['from_messages'])
-        to_poi= float( my_dataset[i]['from_this_person_to_poi'])
-        
-        from_poi = float(my_dataset[i]['from_poi_to_this_person'])
-        totalMessages = x + y
-
-
-        my_dataset[i]['to_poi_percentage'] = 0
-        my_dataset[i]['from_poi_percentage'] = 0
-        if totalMessages > 0:
-            my_dataset[i]['to_poi_percentage'] = to_poi/totalMessages
-            my_dataset[i]['from_poi_percentage'] = from_poi/totalMessages
-
-    #----------------------------------------
-
-    ### Extract features and labels from dataset for local testing
-    data = feature_format.featureFormat(my_dataset, features_list, sort_keys = True)
-    labels, features = feature_format.targetFeatureSplit(data)
-
-    return labels, features
-
-
-
-
-
-#%% test3
-
-lis = [[],0,1]
-print lis
-
-lis[0]= ['sdf','sdf']
-
-print lis
-print 'end of test3 cell'
+# useGaussianNBKfold()
+print 'end of cell 11'
