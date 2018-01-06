e5a02ab2da4f8312b064cfbbb19b0cf46277a5ac
shakir <alshakir_sa@yahoo.com>
2018-01-06 23:21:15 +0300

    finalizing the code

--- poi_id_final.py
+++ poi_id_final.py
@@ -12,10 +12,7 @@ There are 3 scenarios (Linux, Mac, and windows 10)
 
 ------- Secnario one (linux) ----------
 sys.path.append("../tools/")
-
 ------- End of Secnario one (linux) ----------
-
-
 
 ------- Secanrio two and three (Win10 and Mac OS) ----------
 
@@ -36,8 +33,6 @@ pParent = dirName[:dirName.rindex('\\')+1]
 pParent = dirName[:dirName.rindex('/')+1]
 
 ------- End of Secanrio two and three (Win10 and Mac OS) ----------
-
-
 
 
 toolsPath = pParent+'tools'
@@ -85,7 +80,7 @@ with open( "final_project_dataset.pkl", "r") as data_file:
 
 
 ### Task 3: Create new feature(s)
-### we will create two new features that are the percentage of the total email that the person send to or recieve from POI:
+### we will create two new features that are the percentage of the total email that the person sent to or recieved from POI:
 #           'to_poi_percentage',
 #           'from_poi_percentage'
 
@@ -122,12 +117,8 @@ labels, features = feature_format.targetFeatureSplit(data)
 ### Task 4: Try a varity of classifiers
 
 
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
 
-# Provided to give you a starting point. Try a variety of classifiers.
+
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -142,135 +133,115 @@ features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.25, random_state=42)
 
 
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
-### check your results. You do not need to change anything below, but make sure
-### that the version of poi_id.py that you submit can be run on its own and
-### generates the necessary .pkl files for validating your results.
-
-print 'end of cell 6'
-
-
-
-### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
-
-# Provided to give you a starting point. Try a variety of classifiers.
-
-
-
-
-
-
-#%%
-print features_list
-
-#%% cell 11
-# testing gaussianNB with kfold
-
-
-# print "This is the useGaussianNBKfold() method"
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import KFold,StratifiedKFold
 
 from sklearn.cross_validation import cross_val_score
 
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score, precision_score
 import numpy as np
 
 
 kf = KFold(n_splits=7,shuffle=True)
 clf = GaussianNB()
-# print type(features)
-
-# print '-----------\n\n\n'
-
-# print cross_val_score(clf,features,labels,scoring=None,cv=7)
-# print '-----------\n\n\n'
-# print '-----------\n\n\n'
-
-
-
-dump_classifier_and_data(clf, my_dataset, features_list)
-
 
 f = np.array(features)
 l = np.array(labels)
-# print f.shape
-# print l.shape
 
-recList = []
-presList = []
+recallList = []
+precisionList = []
 
 for train_index, test_index in kf.split(features):
-    # print train_index
-    # print '====='
-    # print test_index
-    # print'*********\n\n'
     features_train = f[train_index]
     labels_train = l[train_index]
     features_test = f[test_index]
     labels_test = l[test_index]
 
-
     clf.fit(features_train,labels_train)
 
     pred = clf.predict(features_test)
 
-
     score = accuracy_score(labels_test,pred)
 
-    prec_reca_f = precision_recall_fscore_support(labels_test,pred)
-
-    # print score
-    # print '----------------'
-
-    # print prec_reca_f
-    # print 'end of cell 6'
-
-    rec =  recall_score(labels_test,pred)
-    pres = precision_score(labels_test, pred)
-    print rec, ' is the recall'
-    print pres, 'is the precision'
+    recall =  recall_score(labels_test,pred)
+    precision = precision_score(labels_test, pred)
+    print '**** New Fold ****'
+    print recall, ' is the recall'
+    print precision, 'is the precision'
+    print 
 
 
-    recList.append(rec)
-    presList.append(pres)
+
+    recallList.append(recall)
+    precisionList.append(precision)
 
 
-    # print labels_test[7]
-    # print pred[7]
-    counter = 0
-    wrongcount=0
+avgRecall = np.average(recallList)
+avgPrecision = np.average(precisionList)
 
-    cc = 1
-
-    # for i in labels_test:
-    #     if i == 1 or pred[counter]== 1 :
-    #         print 'i = ', i,  ' ___pred = ', pred[counter]
-    #         print cc
-    #         cc+=1
-    #     # if i == pred[counter]:
-    #     #     print i, '****', pred[counter]
-    #     #     print type(i),'type***', type(pred[counter])
-    #     #     print counter
-    #     # else:
-    #     #     wrongcount+=1
-    #     counter+=1
-
-    # print counter
-    # print 'wrong count = ', wrongcount
-avgRecall = np.average(recList)
-avgPrecision = np.average(presList)
-
-
+print '=============Final Result=================='
 print "average recall =  ", avgRecall
 print "average precision  = " , avgPrecision
 
 
-# useGaussianNBKfold()
-print 'end of cell 11'
+
+
+
+
+
+'''
+Example run  of this code
+
+**** New Fold ****
+0.4  is the recall
+1.0 is the precision
+
+**** New Fold ****
+1.0  is the recall
+0.5 is the precision
+
+**** New Fold ****
+0.333333333333  is the recall
+1.0 is the precision
+
+**** New Fold ****
+0.333333333333  is the recall
+0.5 is the precision
+
+**** New Fold ****
+0.5  is the recall
+0.333333333333 is the precision
+
+=============Final Result==================
+average recall =   0.366666666667
+average precision  =  0.47619047619
+
+
+
+
+
+
+example result of the tester.py
+GaussianNB(priors=None)
+	Accuracy: 0.85707	Precision: 0.49964	Recall: 0.34350	F1: 0.40711	F2: 0.36640
+	Total predictions: 14000	True positives:  687	False positives:  688	False negatives: 1313	True negatives: 11312
+
+
+'''
+
+
+
+
+
+
+### Task 6: Dump your classifier, dataset, and features_list so anyone can
+### check your results. You do not need to change anything below, but make sure
+### that the version of poi_id.py that you submit can be run on its own and
+### generates the necessary .pkl files for validating your results.
+
+dump_classifier_and_data(clf, my_dataset, features_list)
+
+
+
